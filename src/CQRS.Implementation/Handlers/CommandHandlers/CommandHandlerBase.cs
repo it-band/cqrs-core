409bb8a9ca2a5c79dd96bbaccb5d6d48000dec46
Denis Novogrodsky <denis.novogrodsky@ids-group.co.uk>
2019-05-16 17:31:48 +0300

    fix base Command and Query handler

--- src/CQRS.Implementation/Handlers/CommandHandlers/CommandHandlerBase.cs
+++ src/CQRS.Implementation/Handlers/CommandHandlers/CommandHandlerBase.cs
@@ -1,12 +1,12 @@
 ﻿using System.Threading.Tasks;
 using CQRS.Abstractions;
-using CQRS.Implementation.Commands;
+using CQRS.Abstractions.Models;
 using CQRS.Models;
 
 namespace CQRS.Implementation.Handlers.CommandHandlers
 {
     public abstract class CommandHandlerBase<TIn, TOut> : ICommandHandler<TIn, Task<Result<TOut>>>
-        where TIn : CommandBase<TOut>
+        where TIn : ICommand<Task<Result<TOut>>>
     {
         public abstract Task<Result<TOut>> Handle(TIn input);
     }
--- src/CQRS.Implementation/Handlers/QueryHandlers/QueryHandlerBase.cs
+++ src/CQRS.Implementation/Handlers/QueryHandlers/QueryHandlerBase.cs
@@ -1,12 +1,13 @@
 ﻿using System.Threading.Tasks;
 using CQRS.Abstractions;
+using CQRS.Abstractions.Models;
 using CQRS.Implementation.Queries;
 using CQRS.Models;
 
 namespace CQRS.Implementation.Handlers.QueryHandlers
 {
     public abstract class QueryHandlerBase<TIn, TOut> : IQueryHandler<TIn, Task<Result<TOut>>>
-        where TIn : QueryBase<TOut>
+        where TIn : IQuery<Task<Result<TOut>>>
     {
         public abstract Task<Result<TOut>> Handle(TIn input);
     }
